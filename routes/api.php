8dc64ea1127f25892506757ff0c93b80c2204177
esarong <esarong@student.apc.edu.ph>
2023-12-08 17:14:26 +0800

    Setting Restful Api Routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource(name:'/users', controller:UserController::class);
+
+Route::apiResource(name:'/userstatuses', controller:UserStatusController::class);
+
+Route::apiResource(name:'/transactions', controller:TransactionController::class);
+
+Route::apiResource(name:'/transaction_items', controller:TransactionItemController::class);
+
+Route::apiResource(name:'/inventory', controller:InventoryController::class);
+
+Route::apiResource(name:'/inventory_category', controller:InventoryCategoryController::class);
+
+Route::apiResource(name:'/inventory_unit', controller:InventoryUnitController::class);
+
+Route::apiResource(name:'/supplier', controller:SupplierController::class);
